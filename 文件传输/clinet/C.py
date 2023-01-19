93b3ccec6b99403dae73d6442c749b297a14b39d
CzzzzzzJ <427999001@qq.com>
2023-01-19 14:43:25 +0800

    添加文件传输功能

---
+++ 文件传输/clinet/C.py
@@ -0,0 +1,38 @@
+import json
+import socket
+import struct
+
+download_dir = r"C:\Users\czj\Desktop\python\socket111\文件传输\clinet\download\/"
+#  买手机
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 拨号
+phone.connect(("127.0.0.1", 8088))
+# 发收消息
+while True:
+    # 发送命令
+    msg = input(">>").strip()  # 去空格
+    if not msg: continue
+    phone.send(msg.encode("utf-8"))
+    # 接受文件内容，以”写“的方式打开新文件并写入
+    # 先取报头长度
+    obj = phone.recv(4)
+    header_len = struct.unpack("i", obj)[0]
+    # 再收报头
+    header_bytes = phone.recv(header_len)
+    # 从报头中解析信息
+    header_json = header_bytes.decode("utf-8")
+    header_dic = json.loads(header_json)
+    total_size = header_dic["file_size"]
+    filename = header_dic["filename"]
+
+    # 接收真实信息
+    with open('%s%s' % (download_dir, filename), "wb") as f:
+        recv_size = 0
+        while recv_size < total_size:
+            line = phone.recv(1024)
+            f.write(line)
+            recv_size += len(line)
+            print('总大小：%s  已下载：%s' % (total_size, recv_size))
+
+# 关闭
+phone.close()
